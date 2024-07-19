98a70ff7bcbf60a8c88e1707214444850805ec94
talbdbd <bendavid.bd.tal@gmail.com>
2024-07-18 19:40:07 -0700

    refactored css

---
+++ src/pages/AboutMePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AboutMe from '../components/AboutMe';
+
+const AboutMePage = () => (
+  <main>
+    <AboutMe />
+  </main>
+);
+
+export default AboutMePage;
---
+++ src/pages/PortfolioPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Portfolio from '../components/Portfolio';
+
+const PortfolioPage = () => (
+  <main>
+    <Portfolio />
+  </main>
+);
+
+export default PortfolioPage;
